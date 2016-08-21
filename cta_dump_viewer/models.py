d5321cce0e32472a2225280865215e1734908694
Tristan Crockett <tristan.h.crockett@gmail.com>
2016-08-21 13:50:33 -0500

    Add reading schema without GeoDjango for now

--- cta_dump_viewer/models.py
+++ cta_dump_viewer/models.py
@@ -2,4 +2,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Reading(models.Model):
+    serial_number = models.IntegerField()
+    route = models.CharField(max_length=10, db_index=True)
+    direction = models.CharField(max_length=5, db_index=True)
+    vehicle_number = models.CharField(max_length=10)
+    date = models.DateField(db_index=True)
+    time = models.TimeField(db_index=True)
+    passengers_on = models.IntegerField()
+    passengers_off = models.IntegerField()
+    passengers_in = models.IntegerField()
+    latitude = models.CharField(max_length=10)
+    longitude = models.CharField(max_length=10)
