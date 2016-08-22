85336631a71d3c1bcc2344eae6ecf7ba2dc497a7
Tristan Crockett <tristan.h.crockett@gmail.com>
2016-08-21 22:03:36 -0500

    Show snapped points and representative ways

--- core/services/osrm_matcher.py
+++ core/services/osrm_matcher.py
@@ -5,6 +5,7 @@
 import logging
 import requests
 from django.conf import settings
+from cta_dump_viewer.models import OsmWayNode
 
 logger = logging.getLogger(__name__)
 
@@ -18,14 +19,13 @@ def match_response(coord_string, radius_string):
     }
     request_url = '{}/{}'.format(settings.OSRM.MATCH_ENDPOINT, coord_string)
     logger.debug('Request url: {}'.format(request_url))
-    print options
     response = requests.get(request_url, params=options)
     return response.json()
 
 
-def match(lats, lons, radiuses):
+def match(coords, radiuses):
     coord_string = ';'.join(
-        "%s,%s" % (lon, lat) for lat, lon in zip(lats, lons)
+        "%s,%s" % (lon, lat) for lon, lat in coords
     )
     output = match_response(coord_string, ';'.join(map(str, radiuses)))
     logger.debug('Match response: {}'.format(output))
@@ -38,3 +38,63 @@ def match(lats, lons, radiuses):
             logger.warning('Tracepoint index {} not found'.format(index))
             unsnapped_points.append(index)
     return output, unsnapped_points
+
+
+def naive_match(coords):
+    radiuses = [settings.OSRM.DEFAULT_RADIUS] * len(coords)
+    output, unsnapped_points = match(coords, radiuses)
+    snapped_points = [
+        tracepoint['location']
+        for tracepoint
+        in output['tracepoints']
+        if tracepoint
+    ]
+    nodes = []
+    for tracepoint in output['tracepoints']:
+        if tracepoint:
+            route = output['matchings'][tracepoint['matchings_index']]
+            legs = route['legs']
+            if tracepoint['waypoint_index'] == len(legs):
+                continue
+            leg = legs[tracepoint['waypoint_index']]
+            leg_nodes = leg['annotation']['nodes']
+            for node in leg_nodes:
+                if node not in nodes:
+                    nodes.append(node)
+
+            way_nodes_lookup = {}
+            for i in range(len(nodes)-1):
+                begin_node = nodes[i]
+                end_node = nodes[i+1]
+                way_nodes = OsmWayNode.objects.raw("""
+                    select
+                        n.id osm_node_id,
+                        osm_way_id,
+                        st_x(st_transform(
+                            st_geomfromtext('point('||n.lon/100||' '||n.lat/100||')', 3785),
+                            4326
+                        )) longitude,
+                        st_y(st_transform(
+                            st_geomfromtext('point('||n.lon/100||' '||n.lat/100||')', 3785),
+                            4326
+                        )) latitude
+                    from
+                    (
+                        select id osm_way_id, unnest(nodes) node_id from planet_osm_ways where id =
+                        (
+                            select id
+                            from planet_osm_ways
+                            where nodes @> ARRAY[{}::bigint]
+                            and nodes @> ARRAY[{}::bigint]
+                            order by array_length(nodes, 1)
+                            limit 1
+                        )
+                    ) nodes
+                    join planet_osm_nodes n on (node_id = n.id)
+                """.format(begin_node, end_node))
+                if len(list(way_nodes)) > 0:
+                    way_id = way_nodes[0].osm_way_id
+                    if way_id not in way_nodes_lookup:
+                        way_nodes_lookup[way_id] = way_nodes
+
+    return snapped_points, way_nodes_lookup
--- cta_dump_viewer/views.py
+++ cta_dump_viewer/views.py
@@ -3,15 +3,33 @@ from django.template import Context
 from django.utils.safestring import mark_safe
 import geojson
 from cta_dump_viewer.models import Reading
+from core.services.osrm_matcher import naive_match
+
+
+def points(serial_number):
+    return [
+        (float(reading.longitude), float(reading.latitude)) for reading in
+        Reading.objects.filter(serial_number=serial_number).all()
+    ]
 
 
 def index(request):
-    raw = geojson.LineString([
-        (float(reading.longitude), float(reading.latitude)) for reading in
-        Reading.objects.filter(serial_number=48661914).all()
+    raw_points = points(48661914)
+    raw = geojson.LineString(raw_points)
+    matched_points, way_nodes_lookup = naive_match(raw_points)
+    ways = geojson.GeometryCollection([
+        geojson.LineString([(node.longitude, node.latitude) for node in way_nodes])
+        for way_nodes in way_nodes_lookup.values()
     ])
+    snapped = geojson.GeometryCollection(
+        [geojson.Point(coord) for coord in matched_points]
+    )
     return render(
         request,
         'cta_dump_viewer/index.html',
-        Context({'raw_geojson': mark_safe(raw)})
+        Context({
+            'raw_geojson': mark_safe(raw),
+            'snapped_geojson': mark_safe(snapped),
+            'ways_geojson': mark_safe(ways)
+        })
     )
