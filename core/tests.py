df4d23bfd57e50ca4b05be40f5e943315a9b258c
Tristan Crockett <tristan.h.crockett@gmail.com>
2016-08-21 23:57:47 -0500

    test_match_response

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from unittest import TestCase
+import json
+import os
+import responses
+import urlparse
+from django.conf import settings
+from core.services.osrm_matcher import match_response
 
-# Create your tests here.
+
+class OsrmMatcherTestCase(TestCase):
+    def test_match_response(self):
+        coords = 'coords'
+        radius = 'radiuses'
+        with responses.RequestsMock() as rsps:
+            response = '{"error": "not found"}'
+            rsps.add(
+                responses.GET,
+                os.path.join(settings.OSRM.MATCH_ENDPOINT, coords),
+                body=response,
+                status=200,
+                content_type='application/json'
+            )
+            self.assertEqual(
+                match_response(coords, radius),
+                json.loads(response)
+            )
+
+            self.assertEqual(len(rsps.calls), 1)
+
+            parsed_params = urlparse.parse_qs(
+                urlparse.urlparse(rsps.calls[0].request.url).query
+            )
+            self.assertEqual(parsed_params['radiuses'], ['radiuses'])
+            self.assertEqual(parsed_params['annotations'], ['true'])
